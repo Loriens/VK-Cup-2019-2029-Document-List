453d41b26313b50c9edd592b937f2d9965fa86d5
Vladislav Markov <vladislavmarkov18@gmail.com>
2020-02-15 17:42:34 +0300

    Initial Commit

---
+++ Base Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Base Project
+//
+//  Created by Vladislav on 15.02.2020.
+//  Copyright © 2020 Vladislav Markov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
