75724c6c5cc93fb20195ca6d8ed342044f2b16b5
Vladislav Markov <vladislavmarkov18@gmail.com>
2020-02-16 15:54:39 +0300

    Rename project

--- VK Document List/Application Layer/AppConfiguration.swift
+++ VK Document List/Application Layer/AppConfiguration.swift
@@ -1,6 +1,6 @@
 //
 //  AppConfiguration.swift
-//  Base Project
+//  VK Document List
 //
 //  Created by Vladislav on 15.02.2020.
 //  Copyright © 2020 Vladislav Markov. All rights reserved.
--- VK Document List/Presentation Layer/User Stories/Initial/InitialViewController.swift
+++ VK Document List/Presentation Layer/User Stories/Initial/InitialViewController.swift
@@ -1,6 +1,6 @@
 //
 //  InitialViewController.swift
-//  Base Project
+//  VK Document List
 //
 //  Created by Vladislav on 15.02.2020.
 //  Copyright © 2020 Vladislav Markov. All rights reserved.
